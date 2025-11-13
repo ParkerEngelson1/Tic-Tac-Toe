d4002279ca0a062a6ba921b1342fc09362afee43
Parker <Parker.engelson09@gmail.com>
2025-11-13 09:15:05 -0800

    Final Product

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -14,6 +14,9 @@ struct ContentView: View {
     @State private var winMessage = ""
     var body: some View {
         VStack {
+            Text("Tic Tac Toe")
+                .bold()
+                .font(.largeTitle)
             LazyVGrid(columns: Array(repeating: GridItem(.fixed(120)), count: 3)) {
                 ForEach(0..<9) { index in
                     ZStack{
@@ -24,39 +27,25 @@ struct ContentView: View {
                             .font(.system(size: 90))
                             .fontWeight(.heavy)
                     }
-                    
                     .frame(width: 120, height: 120, alignment: .center)
                     .cornerRadius(30)
                     .onTapGesture {
                         withAnimation{
                             if moves[index] == "" {
-                                
-                                
                                 moves[index] = xTurn ? "X" : "O"
                                 xTurn.toggle()
                             }
                         }
                     }
-                    
                     .rotation3DEffect(.degrees(moves[index] == "" ? 180 : 0), axis: (0, 1, 0))
-                    
-                    
-                    
                 }
-                
-                
             }
-            Text("Tic Tac Toe")
-                .bold()
-                .font(.largeTitle)
-            
         }
         .preferredColorScheme(.dark)
         .alert(isPresented: $gameOver) {
             Alert(title: Text(winMessage), dismissButton: .destructive(Text("Play Again"), action: {
                 withAnimation {
                     moves = Array(repeating: "", count: 9)
-                   
                     gameOver = false
                     winMessage = ""
                 }
@@ -64,36 +53,34 @@ struct ContentView: View {
         }
         .onChange(of: moves) {
             oldValue, newValue in checkForWinner()
-            
-            
         }
     }
     private func checkForWinner() {
-       
+        
         checkLine(a: 0, b: 1, c: 2) // first row
         checkLine(a: 3, b: 4, c: 5) // second row
         checkLine(a: 6, b: 7, c: 8) // third row
-
+        
         
         checkLine(a: 0, b: 3, c: 6) // first column
         checkLine(a: 1, b: 4, c: 7) // second column
         checkLine(a: 2, b: 5, c: 8) // third column
-
+        
         
         checkLine(a: 0, b: 4, c: 8) // diagonal
         checkLine(a: 2, b: 4, c: 6) // diagonal
+        if !(gameOver || moves.contains("")){
+            winMessage = "Cats Game"
+            gameOver = true
+        }
     }
-
     private func checkLine(a: Int, b: Int, c: Int) {
         if moves[a] != "" && moves[a] == moves[b] && moves[b] == moves[c] {
             winMessage = "\(moves[a]) is the winner!"
             gameOver = true
         }
     }
-
-
 }
-
 #Preview {
     ContentView()
 }
